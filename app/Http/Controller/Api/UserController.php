a121670b91e56b8757995d722f677a94d188506c
Gabriel Koerich <gabriel@bulldesk.com.br>
2021-06-06 05:08:55 -0300

    Great user api controller

---
+++ app/Http/Controller/Api/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controller\Api;
+
+use App\User\Model\JsonPlaceholderUserRepository;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+
+class UserController
+{
+    use ValidatesRequests;
+
+    /**
+     * Create a new instance.
+     */
+    public function __construct(private JsonPlaceholderUserRepository $users)
+    {
+        //
+    }
+
+    /**
+     * List all resources.
+     */
+    public function index()
+    {
+        return $this->users->all();
+    }
+}
