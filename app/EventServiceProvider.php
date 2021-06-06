3f17596694d4a5810aa1b9bf83dc123355d9cde3
Gabriel Koerich <gabriel@bulldesk.com.br>
2021-06-06 04:02:12 -0300

    Modify structure

--- app/EventServiceProvider.php
+++ app/EventServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Providers;
+namespace App;
 
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
--- config/app.php
+++ config/app.php
@@ -169,12 +169,9 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-
+        App\AppServiceProvider::class,
+        App\EventServiceProvider::class,
+        App\RouteServiceProvider::class,
     ],
 
     /*
