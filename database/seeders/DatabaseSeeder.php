ee6e7cf75d966a9498a65bd3d61505292bde721a
Gabriel Koerich <gabriel@bulldesk.com.br>
2021-06-08 12:30:34 -0300

    Create user and token

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,17 +2,23 @@
 
 namespace Database\Seeders;
 
+use Hash;
 use Illuminate\Database\Seeder;
+use App\User\Model\InternalUser;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
-     *
-     * @return void
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $user = InternalUser::factory()->create();
+
+        $token = $user->createToken('Default Token');
+
+        dump(
+            'Token for requests:', $token->plainTextToken
+        );
     }
 }
